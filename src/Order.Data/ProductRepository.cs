13cd0327d28a1c104d1ccd5c30a475d4200427b9
Radek Necas <radnecx@gmail.com>
2021-11-21 18:33:00 +0100

    Add product repository and specification for products

---
+++ src/Order.Data/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Data.Entities;
+using Order.Data.Specifications;
+using Order.Data.Specifications.Evaluators;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly OrderContext _orderContext;
+        private readonly ISpecificationEvaluator _specificationEvaluator;
+
+        public ProductRepository(OrderContext orderContext, ISpecificationEvaluator specificationEvaluator)
+        {
+            _orderContext = orderContext;
+            _specificationEvaluator = specificationEvaluator;
+        }
+
+        public async Task<IReadOnlyList<OrderProduct>> GetProducts(ISpecification<OrderProduct> specification)
+        {
+            var query = EvaluateSpecification(specification);
+            return await query.ToListAsync();
+        }
+
+        private IQueryable<OrderProduct> EvaluateSpecification(ISpecification<OrderProduct> specification)
+        {
+            return _specificationEvaluator.EvaluateSpecification(specification, _orderContext.Set<OrderProduct>());
+        }
+    }
+}
+
---
+++ src/Order.Data/Specifications/ProductsByNamesSpecification.cs
@@ -0,0 +1,16 @@
+﻿using Order.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Order.Data.Specifications
+{
+    public class ProductsByNamesSpecification : BaseEmptySpecification<OrderProduct>
+    {
+        public ProductsByNamesSpecification(IEnumerable<string> productNames)
+        {
+            Query = p => productNames.Contains(p.Name);
+            OrderBy = p => p.Name;
+        }
+    }
+}
